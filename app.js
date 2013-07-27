42f1fd68cefc535076fd1d551f99002f1e4f4a7e
tq-jappy <tq-ed@v7.com>
2013-07-27 21:23:35 +0900

    move configuration from app.js to config folder.

--- app.js
+++ app.js
@@ -4,30 +4,13 @@
  */
 
 var express = require('express')
-  , partials = require('express-partials')
   , http = require('http')
   , path = require('path')
-  , config = require('./config/config');
+  , config = require('./config/config')['development'];
 
 var app = express();
 
-app.use(partials());
-
-// all environments
-app.set('port', config.server.port);
-app.set('views', __dirname + '/app/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+require('./config/express')(app, config);
 
 require('./config/routes')(app);
 
@@ -38,5 +21,7 @@ var server = http.createServer(app).listen(app.get('port'), function(){
 var sio = require('socket.io').listen(server);
 app.set('sio', sio);
 
-var  messageController = require('./app/controllers/message_controller').messageController(app);
+var  messageController = require('./app/controllers/socket_controller').messageController(app);
 sio.sockets.on('connection', messageController.connection);
+
+exports = module.exports = app;
